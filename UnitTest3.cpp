2df67fb1d01214f69cc6720611f4d46ab72eed6a
sofiia-pavl <72608224+sofiia-pavl@users.noreply.github.com>
2021-03-01 18:08:12 +0200

    Add files via upload

---
+++ UnitTest3.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab1,5/Traid.h"
+#include"C:\Users\Софія\source\repos\lab1,5\lab1,5\Traid.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest3
+{
+	TEST_CLASS(UnitTest3)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Triad h;
+			bool d = h.Init(2, 5, 4);
+			Assert::AreEqual(d, true);
+		}
+	};
+}
